337b5a2a3e2c6a21d39634d004d6042a29a16937
Anthony Nathan <anthony.nathan@standardbank.co.za>
2022-05-06 00:22:22 +0200

    Initial Commit

---
+++ src/main/java/za/co/absa/api/bookstore/SimpleBookStoreApplication.java
@@ -0,0 +1,13 @@
+package za.co.absa.api.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/absa/api/bookstore/SimpleBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.absa.api.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
