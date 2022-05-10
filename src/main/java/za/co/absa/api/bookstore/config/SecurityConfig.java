ccd6bbc84a8e32eb76127ff5a8a5468b300e265c
Anthony Nathan <anthony.nathan@standardbank.co.za>
2022-05-10 14:38:13 +0200

    Finished implementing security

--- src/main/java/za/co/absa/api/bookstore/config/SecurityConfig.java
+++ src/main/java/za/co/absa/api/bookstore/config/SecurityConfig.java
@@ -2,19 +2,20 @@ package za.co.absa.api.bookstore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+    SecurityWebFilterChain configure(ServerHttpSecurity http) {
         http
                 .authorizeExchange()
                 .pathMatchers("/actuator/**", "/v3/api-docs/**", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html")
                 .permitAll()
-                .anyExchange().authenticated().and().oauth2Login();
+                .anyExchange().authenticated().and().oauth2Login().and().csrf().disable();
 
         return http.build();
     }
